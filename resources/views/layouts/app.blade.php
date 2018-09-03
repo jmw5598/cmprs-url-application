dd5e145d4f129e4d3c7718512820bd21b82ef064
Jason White <jmw5598@gmail.com>
2018-09-03 08:55:32 -0400

    cleaned up app.blade.php, add app.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Laravel Quickstart - Basic</title>
+    <title>Compress | For lengthy link urls</title>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <style>
       @import url('https://use.fontawesome.com/releases/v5.3.1/css/all.css');
       @import url('https://fonts.googleapis.com/css?family=Pacifico');
       @import url('https://fonts.googleapis.com/css?family=Encode+Sans+Condensed');
     </style>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
   </head>
 
   <body>
-    <div class="container">
-      <nav class="navbar navbar-default">
-                <!-- Navbar Contents -->
-      </nav>
-      <div class="row">
-        <div class="col-sm-12">
-          @yield('content')
-        </div>
-      </div>
-    </div>
+
+    @yield('content')
+
   </body>
 </html>
