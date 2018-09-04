395c51fca50a9fe467ccabf958ca15a864d57b2d
Jason White <jmw5598@gmail.com>
2018-09-04 11:55:48 -0400

    updated home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,24 +4,28 @@
 
 <div class="container">
 
+  @include('common.errors')
+
   <!-- Heading -->
   <h1 class="compress-title">
     <span class="icon">&#8677;</span> Compress <span class="icon">&#8676;</span>
   </h1>
 
   <!-- Form -->
-  <form class="compress-form">
+  <form action="/" method="POST" class="compress-form">
+    {{ csrf_field() }}
     <div class="form-group">
-      <input type="text" class="form-control" placeholder="Type your lengthy url..." />
-      <button role="button" class="btn-compress">&rarr;</button>
+      <input type="text" name="url" id="url" class="form-control" placeholder="Type your lengthy url..." />
+      <button role="button" class="btn-compress">&#9654;</button>
     </div>
   </form>
 
   <!-- Result -->
-  <div class="compress-result">
-    http://cmprs.xyz/593f4a68
-  </div>
-
+  @if(!empty($redirect))
+    <div class="compress-result">
+      <a href="{{ $redirect->base . $redirect->hash }}" target="_blank">{{ $redirect->base . $redirect->hash }}</a>
+    </div>
+  @endif
 
 </div>
 
